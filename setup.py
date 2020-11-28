e88669d164886749537adc1fef1f9d2d8b175ab1
Nabil Freij <nabil.freij@gmail.com>
2020-11-28 15:07:43 +0000

    updated CI and py39 tests

--- setup.py
+++ setup.py
@@ -6,21 +6,6 @@ from itertools import chain
 from setuptools.config import read_configuration
 
 ################################################################################
-# Actual setup.py content
-################################################################################
-
-VERSION_TEMPLATE = """
-# Note that we need to fall back to the hard-coded version if either
-# setuptools_scm can't be imported or setuptools_scm can't determine the
-# version, so we catch the generic 'Exception'.
-try:
-    from setuptools_scm import get_version
-    __version__ = get_version(root='..', relative_to=__file__)
-except Exception:
-    __version__ = '{version}'
-""".lstrip()
-
-################################################################################
 # Programmatically generate some extras combos.
 ################################################################################
 extras = read_configuration("setup.cfg")['options']['extras_require']
@@ -34,7 +19,7 @@ ex_extras = dict(filter(lambda i: i[0] not in exclude_keys, extras.items()))
 # Concatenate all the values together for 'all'
 extras['all'] = list(chain.from_iterable(ex_extras.values()))
 
-setup(extras_require=extras,
-      use_scm_version={'write_to': os.path.join('radiospectra', 'version.py'),
-                       'write_to_template': VERSION_TEMPLATE}
-      )
+setup(
+    extras_require=extras,
+    use_scm_version={'write_to': os.path.join('radiospectra', '_version.py')},
+)
