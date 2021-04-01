7af79f2b197c91238c20750c97fb0e14ae51ffdf
Nabil Freij <nabil.freij@gmail.com>
2021-04-01 11:08:57 +0100

    docs update

--- radiospectra/__init__.py
+++ radiospectra/__init__.py
@@ -1,7 +1,28 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+radiospectra
+============
 
-try:
-    from .version import __version__
-except ImportError:
-    __version__ = "unknown"
-__all__ = []
+An open-source Python library for radio spectra in solar physics.
+
+* Homepage: https://sunpy.org
+* Documentation: https://docs.radiospectra.org/en/stable/
+* Source code: https://github.com/sunpy/radiospectra
+"""
+import sys
+
+from .version import version as __version__
+
+# Enforce Python version check during package import.
+__minimum_python_version__ = "3.7"
+
+
+class UnsupportedPythonError(Exception):
+    """Running on an unsupported version of Python."""
+
+
+if sys.version_info < tuple(int(val) for val in __minimum_python_version__.split('.')):
+    # This has to be .format to keep backwards compatibly.
+    raise UnsupportedPythonError(
+        "sunpy does not support Python < {}".format(__minimum_python_version__))
+
+__all__ = ["__version__"]
