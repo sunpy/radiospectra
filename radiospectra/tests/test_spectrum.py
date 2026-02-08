ebaf2776a940b81e9a8c2e2927152b22ad72e490
Monier Ayman <monierayman2007@gmail.com>
2026-02-08 12:22:55 +0200

    Implement freq_at_index method for GenericSpectrogram (fixes #129)

--- radiospectra/tests/test_spectrum.py
+++ radiospectra/tests/test_spectrum.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pytest
 from radiospectra.spectrum import Spectrum
 
 
@@ -7,3 +7,15 @@ def test_spectrum():
     spec = Spectrum(np.arange(10), np.arange(10))
     np.testing.assert_equal(spec.data, np.arange(10))
     np.testing.assert_equal(spec.freq_axis, np.arange(10))
+
+
+def test_freq_at_index():
+    data = [1, 2, 3]
+    freqs = [100, 200, 300]
+    spec = Spectrum(data, freqs)
+    # Test valid indices
+    assert spec.freq_at_index(0) == 100
+    assert spec.freq_at_index(2) == 300
+    # Test out-of-bounds index
+    with pytest.raises(ValueError):
+        spec.freq_at_index(5)
