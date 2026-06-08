f558d2fb04a7e58fcb0ac5cfbec9bb6d34ded106
Amityush-lgtm <kamityush@gmail.com>
2026-06-09 02:03:11 +0530

    Adding tests and updating fits reader for NDA

--- radiospectra/spectrogram/sources/nda.py
+++ radiospectra/spectrogram/sources/nda.py
@@ -10,6 +10,12 @@ class NDASpectrogram(GenericSpectrogram):
     """
     Nançay Decameter Array (NDA) Spectrogram.
 
+    This spectrogram supports data in the ``NewRoutine`` format available here:
+    https://cdn.obs-nancay.fr/repository/nda/newroutine/soleil/
+
+    Frequencies range from approximately 10 MHz to 88 MHz. The data contains
+    two polarization channels (LL and RR).
+
     Examples
     --------
     >>> import radiospectra.net
@@ -18,9 +24,16 @@ class NDASpectrogram(GenericSpectrogram):
     >>> query = Fido.search(a.Time("2025-03-26", "2025-03-27"), a.Instrument("NDA"))  #doctest: +REMOTE_DATA
     >>> downloaded = Fido.fetch(query[0][0])  #doctest: +REMOTE_DATA
     >>> spec = Spectrogram(downloaded[0])  #doctest: +REMOTE_DATA
-    >>> spec  #doctest: +REMOTE_DATA
+    >>> spec[0]  #doctest: +REMOTE_DATA
     <NDASpectrogram NDA, newroutine, ORN 10.010000228881836 MHz - 87.98999786376953 MHz, ...>
     """
+
+    def __init__(self, data, meta, **kwargs):
+        super().__init__(meta=meta, data=data, **kwargs)
+
+    @property
+    def polarisation(self):
+        return self.meta.get("polarisation", "Unknown")
 
     @property
     def observatory_location(self):
