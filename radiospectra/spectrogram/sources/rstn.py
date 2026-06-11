fe184117073364780aadd7ae514b334710b41178
hayesla <lauraannhayes@gmail.com>
2026-06-11 15:09:36 +0100

    fixing silly things

--- radiospectra/spectrogram/sources/rstn.py
+++ radiospectra/spectrogram/sources/rstn.py
@@ -13,8 +13,8 @@ class RSTNSpectrogram(GenericSpectrogram):
     >>> from sunpy.net import Fido, attrs as a
     >>> from radiospectra.spectrogram import Spectrogram
     >>> query = Fido.search(a.Time('2017/09/07 00:00', '2017/09/07 23:00'), a.Instrument.rstn)  #doctest: +REMOTE_DATA
-    >>> downloaded = Fido.fetch(query[0][0])  #doctest: +REMOTE_DATA
-    >>> spec = Spectrogram(downloaded["rstn"][0])  #doctest: +REMOTE_DATA
+    >>> downloaded = Fido.fetch(query['rstn'][0])  #doctest: +REMOTE_DATA
+    >>> spec = Spectrogram(downloaded[0])  #doctest: +REMOTE_DATA
     >>> spec  #doctest: +REMOTE_DATA
     <RSTNSpectrogram LEARMONTH, RSTN, RSTN 25000.0 kHz - 180000.0 kHz, 2017-09-06T22:31:51.000 to 2017-09-07T10:06:36.000>
     >>> spec.plot()  #doctest: +REMOTE_DATA
