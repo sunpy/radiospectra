d8ae3983a51c450773803f447971e45c2992c379
Amityush-lgtm <kamityush@gmail.com>
2026-08-05 14:23:57 +0530

    Updating tests

--- radiospectra/spectrogram/tests/test_slicing.py
+++ radiospectra/spectrogram/tests/test_slicing.py
@@ -25,48 +25,6 @@ def spec():
     return GenericSpectrogram(data, meta_dict)
 
 
-def test_slice_by_time_objects(spec):
-    start = spec.times[2]
-    end = spec.times[5]
-    sliced = spec[:, start:end]
-    assert sliced.shape == (5, 3)
-    assert sliced.times[0] == start
-    assert sliced.times[-1] == spec.times[4]
-
-
-def test_slice_by_time_strings(spec):
-    sliced = spec[:, "2021-01-01T00:00:02":"2021-01-01T00:00:05"]
-    assert sliced.shape == (5, 3)
-    assert sliced.times[0] == spec.times[2]
-    assert sliced.times[-1] == spec.times[4]
-
-
-def test_slice_by_frequency_quantities(spec):
-    low = spec.frequencies[1]
-    high = spec.frequencies[3]
-    sliced = spec[low:high, :]
-    assert sliced.shape == (2, 10)
-    assert sliced.frequencies[0] == low
-    assert sliced.frequencies[-1] == spec.frequencies[2]
-
-
-def test_slice_by_time_and_frequency(spec):
-    sliced = spec[spec.frequencies[1] : spec.frequencies[3], spec.times[2] : spec.times[5]]
-    assert sliced.shape == (2, 3)
-
-
-def test_single_time_index(spec):
-    sliced = spec[:, spec.times[2]]
-    assert sliced.shape == (5,)
-    assert (sliced.frequencies == spec.frequencies).all()
-
-
-def test_single_frequency_index(spec):
-    sliced = spec[spec.frequencies[2], :]
-    assert sliced.shape == (10,)
-    assert (sliced.times == spec.times).all()
-
-
 def test_time_profile_method(spec):
     freq = spec.frequencies[2]
     profile = spec.time_profile(freq)
